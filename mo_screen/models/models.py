42f6d20679ec9c682f532e3e1d71efdbcee97ea4
Yousef-Eleshy <yousef.medhat@hotmail.com>
2020-06-04 13:45:47 +0000

    Manufacturing Production

---
+++ mo_screen/models/models.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class mo_screen(models.Model):
+    
+    _inherit = 'mrp.product.produce'
+        
+    size_of_pieces = fields.Char(string='Size of Pieces', readonly=False)
