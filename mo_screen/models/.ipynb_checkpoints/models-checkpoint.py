e73a98e19a8843dce26bceab164a9f8a9b285223
Yousef-Eleshy <yousef.medhat@hotmail.com>
2020-06-07 13:26:10 +0000

    Relation Attempt

--- mo_screen/models/.ipynb_checkpoints/models-checkpoint.py
+++ mo_screen/models/.ipynb_checkpoints/models-checkpoint.py
@@ -8,9 +8,18 @@ class mo_screen(models.Model):
     
     size_of_pieces = fields.Char(string='Size of Pieces', store=True, readonly=False)
     
+    #api.onchange('size_of_pieces')
+    #def _onchange_size(self):
+        # set auto-changing field
+     #   self.size_of_pieces_inv = self.size_of_pieces
 
     
 class inv_screen(models.Model):
     _inherit = 'stock.picking'
     
-    size_of_pieces_inv = fields.Many2one('mo.screen', string='Size of Pieces', store=True, readonly=False)
+    # size_of_pieces_inv = fields.Char(string='Size of Pieces', readonly=False)
+    # size_of_pieces_inv = fields.Many2one('mrp.production', string='Size of Pieces', store=True, readonly=False)
+    
+    size_of_pieces_inv = fields.Many2one(string='Size of Pieces', related='mo_screen.size_of_pieces', store=True)
+    
+    
